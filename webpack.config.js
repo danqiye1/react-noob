ca2e73d18c750873579f078685a078d065c95e7d
danqiye1 <yedanqi@gmail.com>
2018-02-04 21:41:58 +0800

    Set up express for serving index.html

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+import webpack from 'webpack';
+import path from 'path';
+
+export default {
+  entry: [
+    path.join(__dirname, 'app/src/index')
+  ],
+  output: {
+    path: path.join(__dirname, 'app/dist')
+  },
+  module: {
+    rules: [
+      {test: /\.js$/, include: path.join(__dirname, "app/src"), use:['babel']},
+      {test: /\.css$/, loaders: ['style', 'css']}
+    ]
+  },
+};
